7a6444c0abe4794029f230696e29044411256ac4
Quenine <103788113+Quenine@users.noreply.github.com>
2023-01-11 13:08:19 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,79 +1,62 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
-
-dotenv.config()
-
-console.log(process.env.OPENAI_API_KEY)
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hello from ResearchAssistant'
-  })
-})
+app.get("/", (_, res) => res.send({ message: "Hello from ResearchAssistant" }));
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
     let history = req.body.history || [];
-
     if (prompt.toLowerCase().startsWith("debug")) {
       const code = prompt.substring("debug".length).trim();
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `${code}`, `${prompt}`,
+        prompt: `${code}\n${prompt}`,
         temperature: 0.5,
         max_tokens: 3000,
         top_p: 1,
         frequency_penalty: 0.5,
         presence_penalty: 0,
       });
-      res.status(200).send({
-        bot: response.data.choices[0].text
-      });
+      res.status(200).send({ bot: response.data.choices[0].text });
     } else if (history.length > 0 && prompt.toLowerCase().startsWith("history")) {
       const index = parseInt(prompt.substring("history".length).trim());
-      res.status(200).send({
-        bot: history[index]
-      });
+      res.status(200).send({ bot: history[index] });
     } else if (prompt.toLowerCase().startsWith("explain")) {
       const topic = prompt.substring("explain".length).trim();
       const explanation = await generateDetailedExplanation(topic);
-      res.status(200).send({
-        bot: explanation.text,
-        image: explanation.image
-      });
+      res.status(200).send({ bot: explanation.text, image: explanation.image });
     } else {
       history.push(prompt);
       const response = await generateResponseWithHistory(prompt, history);
-      res.status(200).send({
-        bot: response
-      });
+      res.status(201).send({ bot: response });
     }
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log("AI server started on http://localhost:5000"));
 
 async function generateResponseWithHistory(context, history) {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${context}\n${history[history.length - 1]}`,
+    prompt: `${context}\n${history.join("\n")}`,
     temperature: 0.5,
     max_tokens: 3000,
   });
-  return response.data.choices[0].text
+  return response.data.choices[0].text;
+}
+
