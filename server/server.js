26080694ab19f9032cf182f44449273d9d42038c
Quenine <103788113+Quenine@users.noreply.github.com>
2023-01-11 14:09:02 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,62 +1,48 @@
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-const { Configuration, OpenAIApi } = require("openai");
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (_, res) => res.send({ message: "Hello from ResearchAssistant" }));
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from ResearchAssistant!'
+  })
+})
 
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    let history = req.body.history || [];
-    if (prompt.toLowerCase().startsWith("debug")) {
-      const code = prompt.substring("debug".length).trim();
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${code}\n${prompt}`,
-        temperature: 0.5,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-      });
-      res.status(200).send({ bot: response.data.choices[0].text });
-    } else if (history.length > 0 && prompt.toLowerCase().startsWith("history")) {
-      const index = parseInt(prompt.substring("history".length).trim());
-      res.status(200).send({ bot: history[index] });
-    } else if (prompt.toLowerCase().startsWith("explain")) {
-      const topic = prompt.substring("explain".length).trim();
-      const explanation = await generateDetailedExplanation(topic);
-      res.status(200).send({ bot: explanation.text, image: explanation.image });
-    } else {
-      history.push(prompt);
-      const response = await generateResponseWithHistory(prompt, history);
-      res.status(201).send({ bot: response });
-    }
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.5, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error || "Something went wrong");
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
   }
-});
+})
 
-app.listen(5000, () => console.log("AI server started on http://localhost:5000"));
-
-async function generateResponseWithHistory(context, history) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `${context}\n${history.join("\n")}`,
-    temperature: 0.5,
-    max_tokens: 3000,
-  });
-  return response.data.choices[0].text;
-}
-
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
